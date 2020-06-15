0b2a7b925dc7d2e9d96152b079f1cfbdf2b69eaf
ABDULLAH-I-I-I <mmhh26533@yahoo.com>
2020-06-15 23:48:43 +0500

    JS Assignment

---
+++ app.js
@@ -0,0 +1,173 @@
+//  ===== Chapter # 01   Task # 01
+
+// alert("Welcome to JS Progrsmming ");
+
+
+//  ===== Chapter # 01   Task # 02
+
+// alert("Error! Please enter a valid password.");
+
+
+//  ===== Chapter # 01   Task # 03
+
+// alert("Welcome To JS Land..." +  "-\n" + "Happy Coding!");
+
+
+//  ===== Chapter # 01   Task # 04
+
+// alert("Welcome To JS Land...");
+
+
+//  ===== Chapter # 01   Task # 05
+
+// alert("Happy Coding!" +"\n \n  " + "Prevent this page from creating additional dailogs");
+
+
+//  ===== Chapter # 01   Task # 06
+
+// alert("Hello... I can run JS through my web browser's console");
+
+
+//  ===== Chapter # 01   Task # 07
+
+// alert("<html>" + "\n" + "<head>" + "\n\n\n\n\n" + "</head>" + "\n" + "<body>" + "\n\n\n" + "</body>" + "\n" + "</html>");
+
+
+
+
+
+//  ===== Chapter # 02   Task # 01
+
+// var  username =  "A001";
+// alert(username);
+
+//  ===== Chapter # 02   Task # 02
+
+// var myName = "MUHAMMAD ABDULLAH"
+// alert(myName)
+
+//  ===== Chapter # 02   Task # 03
+
+// var message = "Hello World"
+// alert(message)
+
+//  ===== Chapter # 02   Task # 04
+
+// alert("MUHAMMAD ABDULLAH")
+// alert("30 years old")
+// alert("Certified  Mobile  App  Development")
+
+//  ===== Chapter # 02   Task # 05
+
+// alert("PIZZA" + "\n" + "PIZZ" + "\n" + "PIZ" + "\n" + "PI" +"\n" + "P")
+
+//  ===== Chapter # 02   Task # 06
+
+// var email = "My email adress is mmhh26533@yahoo.com"
+// alert(email)
+
+//  ===== Chapter # 02   Task # 07
+
+// var book = "A smarter way to learn JavaScript";
+// alert(book)
+
+//  ===== Chapter # 02   Task # 08
+
+// document.write("Yah! I can write  HTML  content  through JavaScript."   )
+
+//  ===== Chapter # 02   Task # 09
+
+// var c = "====================" + "\n" +"//" + "                                            " + "//" 
+// + "\n" + "//" + "            Pakola                     " +"//" + "====="
+// + "\n" +"//"  + "                                            " +"//" + "            " + "))"
+// + "\n" +"//"  + "                                            " +"//" + "            " + "))"
+// + "\n" +"=========================" +"\n" + "       " + "^^^" + "                                   " + "^^^"   
+// + "\n" + "       " + "@@" + "                                    " + "@@"   
+// alert(c  )
+
+
+
+
+
+
+//  ===== Chapter # 03   Task # 01
+
+// var age = "I  am  30  years  old ";
+// alert(age);
+
+//  ===== Chapter # 03   Task # 02
+
+// var visit = "You  have  visited  this  site   14  times";
+// alert(visit)
+
+//  ===== Chapter # 03   Task # 03
+
+// var birthyear = 1990;
+// document.write("My  birth  year  is  " + birthyear + "<br>" + "Data  type  of  my  declared  variable  is " + typeof birthyear);
+
+//  ===== Chapter # 03   Task # 04
+
+// var  visitor_name  = "ABDULLAH";
+// var  product_title = "T-Shirt";
+// var  quantity = "Ordered 5 T-Shirts";
+// document.write("<b> a. Visitor's  Name: </b>" + "&nbsp" + visitor_name + "<br><br>" 
+// +" <b> b. Product  title: </b> "+"&nbsp&nbsp&nbsp " + product_title
+// + "<br> <br>" + " <b> c. Quantity: </b> "+ "&nbsp  &nbsp  &nbsp  &nbsp  &nbsp  " + quantity + " on XYZ Clothing store.");
+// document.write( "<br><br><br> " +"<b> Abdullah </b>  ordered  <b> 5 T-shirts </b>  on XYZ Clothing store.")
+
+
+
+
+
+//  ===== Chapter # 04   Task # 01
+
+// document.write("<h3> Rules for naming JS variables </h3>" + "<br><br>");
+// document.write(" Variables names can only contain numbers, $ and ___.For example : $my_IstVariable" + " <br> " + 
+// "Variable must begin with a letter, $ or __.For example : $name,_name or name " + " <br> " + 
+// " Variable names are case sensitive " + "<br> " + "  Variable names should not JS keywords "   );
+
+
+
+
+
+
+//  ===== Chapter # 05   Task # 01
+
+
+
+// var a = 5;
+// var b = 10;
+// var c = a+b;
+// document.write("Sum of 5 and 10 is " + c);
+
+//  ===== Chapter # 05   Task # 02
+
+// var a = 5;
+// var b = 10;
+// var c = a-b;
+// document.write("Sub  10 of 5 is " + c);
+
+//  ===== Chapter # 05   Task # 02
+
+// var a = 5;
+// var b = 10;
+// var c = a*b;
+// document.write("Multiply  5 and 10  is " + c);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
